cbf1c6f9b49abdfcac54acb0f7d0de0d4d849a90
Saqib S <msaqibsaeed4@gmail.com>
2019-06-17 18:07:25 +0500

    Updated TODO

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import TodoList from './Components/TodoList';
 
+//TODO: Need to add Bootstrap and add some styling
+
 const destination = document.querySelector('#container');
 
 ReactDOM.render(
